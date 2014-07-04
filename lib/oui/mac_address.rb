b186680692221f0770a33ba7f65803983e478649
Forrest Ye <afu@forresty.com>
2014-07-04 17:58:06 +0800

    parse /w spec

--- lib/oui/mac_address.rb
+++ lib/oui/mac_address.rb
@@ -1,7 +1,17 @@
 module OUI
   class MACAddress
+    attr_accessor :oui, :organization
+
     def self.parse(string)
-      raise "not implemented yet"
+      new(string)
+    end
+
+    def initialize(string)
+      part = '([\d|[a-f]|[A-F]][\d|[a-f]|[A-F]])'
+      string =~ /#{part}[:-]?#{part}[:-]?#{part}[:-]?#{part}[:-]?#{part}[:-]?#{part}/
+
+      @address = "#{$1}:#{$2}:#{$3}:#{$4}:#{$5}:#{$6}"
+      raise 'illegal format' if @address == ':::::'
     end
   end
 end
--- spec/oui/mac_address_spec.rb
+++ spec/oui/mac_address_spec.rb
@@ -3,6 +3,46 @@ require "spec_helper"
 module OUI
   describe MACAddress do
     subject { MACAddress }
+
     it { should respond_to :parse }
+
+    describe "parse" do
+      subject { MACAddress }
+
+      it "parses MAC address like 001122334455" do
+        expect(subject.parse('001122334455')).not_to be_nil
+      end
+
+      it "parses MAC address like 00-11-22-33-44-55" do
+        expect(subject.parse('00-11-22-33-44-55')).not_to be_nil
+      end
+
+      it "parses MAC address like 00:11:22:33:44:55" do
+        expect(subject.parse('00:11:22:33:44:55')).not_to be_nil
+      end
+
+      it "does not parse MAC address like 00:11:22:3344" do
+        expect { subject.parse('00:11:22:3344') }.to raise_error
+      end
+
+      it "parses MAC address like 00:11:22:33:aa:bb" do
+        expect(subject.parse('00:11:22:33:aa:bb')).not_to be_nil
+      end
+
+      it "parses MAC address like 00:11:22:33:AA:BB" do
+        expect(subject.parse('00:11:22:33:AA:BB')).not_to be_nil
+      end
+    end
+
+    describe "instance methods" do
+      subject { MACAddress.new('001122334455') }
+
+      it { should respond_to :oui }
+      it { should respond_to :organization }
+
+      describe "oui" do
+
+      end
+    end
   end
 end
