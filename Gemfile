61778cf120b00aed2a80516bffdbeea044ce5681
Forrest Ye <afu@forresty.com>
2014-07-19 10:00:55 +0800

    add new relic

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gemspec
+
+gem 'newrelic_rpm'
--- lib/oui/server.rb
+++ lib/oui/server.rb
@@ -3,6 +3,10 @@ require "sinatra/json"
 
 module OUI
   class Server < Sinatra::Base
+    configure :production do
+      require 'newrelic_rpm'
+    end
+
     get '/lookup' do
       organization = OUI::MACAddress.parse(params[:mac]).organization
       result = { mac: params[:mac], manufacturer: organization.name, chinese_name: organization.chinese_name }
